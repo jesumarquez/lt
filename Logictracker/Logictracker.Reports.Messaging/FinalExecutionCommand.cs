02d865ec6856783ce1172e1f0a4c380ec6002367
julianmg81 <julian.millan@logictracker.com>
2015-07-03 18:22:38 -0300

    Se incluyen archivos que faltaban FinalExecutionCommand y FussionChartHelpers/ThemeManager.cs
    (cherry picked from commit de867d655301ae4f88a5c44af1cfd6a70f5997ff)

---
+++ Logictracker/Logictracker.Reports.Messaging/FinalExecutionCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Logictracker.Reports.Messaging
+{
+    [Serializable]
+    public class FinalExecutionCommand
+    {
+        public string Email { get; set; }
+        public DateTime InitialDate { get; set; }
+    }
+}
