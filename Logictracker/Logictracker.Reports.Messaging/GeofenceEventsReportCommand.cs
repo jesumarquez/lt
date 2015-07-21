003f7d6383307900153646a2236f2254e01fa607
julianmg81 <julian.millan@logictracker.com>
2015-07-21 16:05:36 -0300

    COmando reporte de geocercas
    (cherry picked from commit a9db708274f5969d4c7b27a089da5f74bd884a2f)

---
+++ Logictracker/Logictracker.Reports.Messaging/GeofenceEventsReportCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logictracker.Reports.Messaging
+{
+    [Serializable]
+    public class GeofenceEventsReportCommand : IReportCommand
+    {
+        public DateTime InitialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+        public string Email { get; set; }
+
+        public int ReportId { get; set; }
+        public int BaseId { get; set; }
+        public int CustomerId { get; set; }
+        public List<int> VehiclesId { get; set; }
+        public string ReportName { get; set; }
+        public List<int> Geofences { get; set; }
+        public bool CalculateKm { get; set; }
+    }
+}
