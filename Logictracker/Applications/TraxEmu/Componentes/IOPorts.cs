9765791e8c13caabfd80409d783b535cf3e5206d
Jose Gutierrez <jagj77@hotmail.com>
2015-07-23 12:34:53 -0300

    + Codigo para el emulador de trax

---
+++ Logictracker/Applications/TraxEmu/Componentes/IOPorts.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraxEmu.Componentes
+{
+
+    class Disparadores
+    {
+        public int[] GSMax = new int[7];
+        public int[] GSMin = new int[7];
+    }
+    class Factors
+    {
+        // Diferencia entre la entrada real y la que se pretende 
+        public int K { get; set; }
+    }
+
+    class IOPorts
+    {
+        public double IN0 { get; set; }
+        public double IN1 { get; set; }
+        public double IN2 { get; set; }
+        public double IN3 { get; set; }
+        public int IN4 { get; set; }
+        public int IN5 { get; set; }
+        public double Contacto { get; set; }
+        public double OUT0 { get; set; }
+        public double OUT1 { get; set; }
+        public double OUT2 { get; set; }
+        public double OUT3 { get; set; }
+        // Volts Bateria principal 00.00
+        public int MainBat { get; set; }
+        // Volts Volts bateria auxiliar 00.00
+        public int AuxBat { get; set; }
+    }
+}
--- Logictracker/Applications/TraxEmu/DirtyParser.cs
+++ Logictracker/Applications/TraxEmu/DirtyParser.cs
@@ -1,23 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TraxEmu.Componentes;
 
 namespace TraxEmu
 {
     class DirtyParser
     {
-        string Parse(string cmd)
+        private IOPorts ports;
+        private Disparadores disparadores;
+
+        public DirtyParser(IOPorts ports)
         {
-            if (cmd.StartsWith(">QGRI"))
-               return ResposeIMEI(cmd);
-            return string.Empty;
+            this.ports = ports;
         }
 
-        private string ResposeIMEI(string cmd)
+        string Parse(string cmd)
         {
-            return "RGRI0123456789012345";
+            return cmd.StartsWith(">Q")
+                ? ProcessQuery(cmd.Substring(2))
+                : (cmd.StartsWith(">S") ? ProcessSet(cmd.Substring(2)) : ResponseError(cmd));
+        }
+
+
+        private string ProcessQuery(string cmd)
+        {
+            var code = cmd.Substring(0, 2);
+            switch (code)
+            {
+                case "AD":
+                    return QAD();
+                case "GS":
+                    return QGS(cmd.Substring(2));
+                case "TD":
+                    return QTD(cmd.Substring(2));
+            }
+        }
+
+        private string QTD(string cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ResponseError(string cmd)
+        {
+            var code = cmd.Substring(0, 2);
+            return "E" + code + ":MSGID";
+        }
+
+        private string ProcessSet(string cmd)
+        {
+            var code = cmd.Substring(0, 2);
+            switch (code)
+            {
+                case "GS":
+                    return SGS(cmd.Substring(2));
+                case "TD":
+                    return STD(cmd.Substring(3));
+            }
+        }
+
+        private string STD(string cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private string SGS(string cmd)
+        {
+            var n = int.Parse(cmd.Substring(0, 2));
+            var min = int.Parse(cmd.Substring(2, 3));
+            var max = int.Parse(cmd.Substring(5, 3));
+            disparadores.GSMax[n] = max;
+            disparadores.GSMin[n] = min;
+            return RGS(n);
+        }
+
+        private string QGS(string cmd)
+        {
+            var n = int.Parse(cmd.Substring(0, 2));
+            return RGS(n);
+        }
+
+        private string RGS(int n)
+        {
+            return string.Format("RGS{0:00}{1:000}{2:000}", n, disparadores.GSMin[n], disparadores.GSMax[n]);
+        }
+
+
+        private string QAD()
+        {
+            return string.Format("RAD00{0:ddMMyyHHmmss}{1:0000}{2:0000}{3:0000}{4:0000}",DateTime.UtcNow,ports.IN4,ports.IN5,ports.AuxBat,ports.MainBat)
         }
     }
 }
