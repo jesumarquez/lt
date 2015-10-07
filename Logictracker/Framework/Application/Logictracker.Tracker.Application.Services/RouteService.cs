d7d16e62c97c3086b64ab272a9e5038fe5ab1252
julianmg <julian.millan@logictracker.com>
2015-10-07 11:02:13 -0300

    Se corrige error de referencias de dll de la Api Rest

--- Logictracker/Framework/Application/Logictracker.Tracker.Application.Services/RouteService.cs
+++ Logictracker/Framework/Application/Logictracker.Tracker.Application.Services/RouteService.cs
@@ -106,7 +106,7 @@ namespace Logictracker.Tracker.Application.Services
             
             if (opened == null) return "ROUTE_CLOSED";
 
-            var ciclo = new CicloLogisticoDistribucion(opened, DaoFactory, new MessageSaver(DaoFactory));
+            var ciclo = new CicloLogisticoDistribucion(opened, DaoFactory,new MessageSaver(DaoFactory));
             var evento = new CloseEvent(DateTime.UtcNow);
             ciclo.ProcessEvent(evento);
             return "CLOG_FINALIZE_SENT";
