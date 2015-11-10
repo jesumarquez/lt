3a3ecdebc5c5bc7a3d2b96a79a46a3f8cb0b5d3e
Jesu Marquez <jesumarquez@hotmail.com>
2015-11-10 17:25:35 -0300

    Cambio el nombre del resource departamentos en el servicio

--- Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/EntitiesService.js
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/EntitiesService.js
@@ -13,7 +13,7 @@ function EntitiesService($resource, $http) {
         },
         resources: {
             bases: $resource("/api/distrito/:distritoId/base/items", { distritoId: "@distritoId" }),
-            departamento: $resource("/api/distrito/:distritoId/base/:baseId/departamento/items", { distritoId: "@distritoId", baseId: "@baseId" }),
+            departamentos: $resource("/api/distrito/:distritoId/base/:baseId/departamento/items", { distritoId: "@distritoId", baseId: "@baseId" }),
             centroDeCostos: $resource("/api/distrito/:distritoId/base/:baseId/centrodecostos/items", { distritoId: "@distritoId", baseId: "@baseId" , deptoId:"@deptoId"}),
             transportista: $resource("/api/distrito/:distritoId/base/:baseId/transportista/items", { distritoId: "@distritoId", baseId: "@baseId" }),
         },
--- Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/rechazo/rechazo-controller.js
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/rechazo/rechazo-controller.js
@@ -3,8 +3,6 @@
     .controller('RechazoController', ['$scope', 'EntitiesService', RechazoController])
     .controller('RechazoItemController',['$scope', 'EntitiesService', RechazoItemController]);
 
-
-
 function RechazoController($scope, EntitiesService) {
 
     $scope.distritoSelected = {};
@@ -29,20 +27,15 @@ function RechazoController($scope, EntitiesService) {
     $scope.desde = new Date();
     $scope.hasta = new Date();
 
-
     $scope.estadoDS = EntitiesService.ticketrechazo.estados.query(null,
         function () { $scope.estadoSelected = $scope.estadoDS[0]; },
         $scope.onerror);
-
 
     $scope.baseDS = EntitiesService.distrito.bases();
     $scope.baseDS.bind("requestEnd", onBaseDSLoad);
     $scope.baseDS.bind("error", onFail);
 
     $scope.departamentoDS = EntitiesService.distrito.departamento();
-    $scope.departamentoDS.bind("requestStart", function (e) {
-        console.log("request started");
-    });
     $scope.departamentoDS.bind("requestEnd", onDepartamentoDSLoad);
     $scope.departamentoDS.bind("error", onFail);
 
@@ -60,7 +53,8 @@ function RechazoController($scope, EntitiesService) {
             $scope.distritoSelected = e.response[0];
         }
     };
-    function onBaseDSLoad(e){
+
+    function onBaseDSLoad(e) {
         if (e.type === "read" && e.response) {
             $scope.baseSelected = e.response[0];
         }
@@ -78,8 +72,8 @@ function RechazoController($scope, EntitiesService) {
 
     function onBasesDSChange(newValue, oldValue) {
         if (newValue !== oldValue) {
-            $scope.departamentoSelected =[];
-            $scope.centroDeCostosSelected =[];
+            $scope.departamentoSelected = [];
+            $scope.centroDeCostosSelected = [];
         }
     };
 
@@ -95,77 +89,78 @@ function RechazoController($scope, EntitiesService) {
                 distritoId: $scope.distritoSelected.Key,
                 baseId: $scope.baseSelected.Key
             });
-               
+
             $scope.transportistaDS = EntitiesService.distrito.transportista.query(
                 {
-                        distritoId: $scope.distritoSelected.Key,
-                        baseId: $scope.baseSelected.Key
+                    distritoId: $scope.distritoSelected.Key,
+                    baseId: $scope.baseSelected.Key
                 }, function () {
-                    $scope.transportistaSelected =[];
+                    $scope.transportistaSelected = [];
 
                 }, function (error) {
                     $scope.notify.show(error.statusText, "error");
 
-            }
+                }
             );
-    }
+        }
     };
 
     function onDepartamentoAndBaseChange(newValue, oldValue) {
         if (newValue[0] !== undefined && newValue[0].length > 0 && newValue != null && newValue !== oldValue)
             $scope.centroDeCostosDS = EntitiesService.distrito.centroDeCostos.query(
             {
-                    distritoId: $scope.distritoSelected.Key,
-                    baseId: $scope.baseSelected.Key,
-                    deptoId: $scope.departamentoSelected.map(function (o) {
-                        return o.Key;
-            })
+                distritoId: $scope.distritoSelected.Key,
+                baseId: $scope.baseSelected.Key,
+                deptoId: $scope.departamentoSelected.map(function (o) {
+                    return o.Key;
+                })
             }, function () {
-                $scope.centroDeCostosSelected =[];
+                $scope.centroDeCostosSelected = [];
             }, function (error) {
                 $scope.notify.show(error.statusText, "error");
-    });
+            });
 
-    $scope.onerror = function (error) {
-        $scope.notify.show(error.statusText, "error");
+        $scope.onerror = function (error) {
+            $scope.notify.show(error.statusText, "error");
+        }
+
+        $scope.rechazosDS = [];
+
+        $scope.gridOptions = {
+            columns:
+            [
+            { field: "Id", title: "Ticket" },
+            { field: "FechaHora", title: "Fecha Hora" },
+            { field: "ClienteCod", title: "Cod. Cliente" },
+            { field: "ClienteDesc", title: "Cliente" },
+            { field: "SupVenDesc", title: "Sup. Venta" },
+            { field: "SupRutDesc", title: "Sup. Ruta" },
+            { field: "UltEstado", title: "Estado" },
+            { field: "Territorio", title: "Territorio" },
+            { field: "Motivo", title: "Motivo" },
+            { field: "Bultos", title: "Bultos" },
+            ]
+        }
+
+        $scope.onNuevo = function () {
+            $scope.rechazoWin.refresh({ url: "Item?op=A" });
+            $scope.rechazoWin.center();
+            $scope.rechazoWin.open();
+        };
+
+        $scope.onEdit = function (id) {
+            $scope.rechazoWin.refresh({ url: "Item?op=E&id=" + id });
+            $scope.rechazoWin.center();
+            $scope.rechazoWin.open();
+        }
+
+        $scope.onBuscar = function () {
+
+        };
+
     }
-
-    $scope.rechazosDS = []; 
-
-    $scope.gridOptions = {
-        columns:
-        [
-        { field: "Id", title: "Ticket" },
-        { field: "FechaHora", title: "Fecha Hora" },
-        { field: "ClienteCod", title: "Cod. Cliente" },
-        { field: "ClienteDesc", title: "Cliente" },
-        { field: "SupVenDesc", title: "Sup. Venta" },
-        { field: "SupRutDesc", title: "Sup. Ruta" },
-        { field: "UltEstado", title: "Estado" },
-        { field: "Territorio", title: "Territorio" },
-        { field: "Motivo", title: "Motivo" },
-        { field: "Bultos", title: "Bultos" },
-        ]
-    }
-
-    $scope.onNuevo = function () {
-        $scope.rechazoWin.refresh({ url: "Item?op=A" });
-        $scope.rechazoWin.center();
-        $scope.rechazoWin.open();
-    };
-
-    $scope.onEdit = function(id) {
-        $scope.rechazoWin.refresh({ url: "Item?op=E&id=" + id });   
-        $scope.rechazoWin.center();
-        $scope.rechazoWin.open();
-    }
-
-    $scope.onBuscar = function() {
-
-    };
-
 }
-
 function RechazoItemController($scope, EntitiesService) {
     $scope.mensaje = "Soy en numero 4";
 }
+
