a6e429462e765ced80821ff146e9d9f3806ce793
julianmg <julian.millan@logictracker.com>
2015-11-18 11:56:06 -0300

    Clases faltantes del modulo de ordenes

---
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/ordenes/ordenes-service.js
@@ -0,0 +1,14 @@
+﻿angular
+    .module('logictracker.ordenes.service', [])
+    .factory('OrdenesService', ['$resource', OrdenesService]);
+
+function OrdenesService($resource) {
+    return $resource("/api/distrito/:distritoId/base/:baseId/ordenes/:id",
+        { distritoId: "@distritoId", baseId: "@baseId", id: "@id" },
+        { query: { method: 'GET',  isArray: true }
+    });
+}
+
+
+
+
