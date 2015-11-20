0b653b0221fc379177c340ae2e3444087c924556
Matias Sisterna <mat_sis@hotmail.com>
2015-11-20 20:53:58 -0300

    Se agrega el Controller Empleado con el fin de poder cargar los combos de supervisor de ventas y supervisor de ruta. Del Punto de Entrega se considera el responsable como la figura de vendedor y los "reporta x" que tenga ese empleado para inferir cual es la jerarquía de supervisores.

--- Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Models/PuntoEntregaModel.cs
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Models/PuntoEntregaModel.cs
@@ -8,6 +8,7 @@ namespace Logictracker.Web.Models
         public string Codigo { get; set; }
         public int PuntoEntregaId { get; set; }
         public string Descripcion { get; set; }
+        public int ResponsableId { get; set; }
     }
 
     public class PuntoEntregaMapper : EntityModelMapper<PuntoEntrega, PuntoEntregaModel>
@@ -17,6 +18,7 @@ namespace Logictracker.Web.Models
             model.Codigo = entity.Codigo;
             model.PuntoEntregaId = entity.Id;
             model.Descripcion = entity.Descripcion;
+            if(entity.Responsable != null) model.ResponsableId = entity.Responsable.Id;
             return model;
         }
 
@@ -27,7 +29,7 @@ namespace Logictracker.Web.Models
 
         public override ItemModel ToItem(PuntoEntrega entity)
         {
-            return new ItemModel { Key = entity.Id, Value = entity.Descripcion};
+            return new ItemModel { Key = entity.Id, Value = entity.Descripcion };
         }
     }
 }
--- Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/rechazo/rechazo-controller.js
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/rechazo/rechazo-controller.js
@@ -200,14 +200,16 @@ function RechazoItemController($scope, EntitiesService) {
     $scope.puntoEntregaRO = true;
 
     $scope.distribucionDS = EntitiesService.distrito.distribuciones.models({ distritoId: $scope.distritoSelected.Key, baseId: $scope.baseSelected.Key }, null, $scope.onFail);
-    $scope.distribucionSeñected = {};
+    $scope.distribucionSelected = {};
     $scope.distribucionRO = true;
 
     $scope.supervisorRutaSelected = {};
     $scope.supervisorRutaRO = true;
+    $scope.supervisorRutaDS = EntitiesService.ticketrechazo.empleadoReporta(onSupervisorRutaDSLoad, onFail);
 
     $scope.supervisorVentasSelected = {};
-    $scope.supervisorVentas = true;
+    $scope.supervisorVentasRO = true;
+    $scope.supervisorVentasDS = EntitiesService.ticketrechazo.empleadoReporta(onSupervisorVentasDSLoad, onFail);
 
     $scope.territorio = "";
     $scope.territorioRO = true;
@@ -218,7 +220,11 @@ function RechazoItemController($scope, EntitiesService) {
     $scope.movimientosDS = {};
 
     $scope.$watch("clienteSelected", onClienteSelected);
+    $scope.$watch("puntoEntregaSelected", onPuntoEntregaSelected);
+    $scope.$watch("supervisorVentasSelected", onSupervisorVentasSelected);
 
+    //$scope.$watch("supervisorVentasDS", onsupervisorVentasDSChange);
+   
     function onClienteSelected(newValue, oldValue) {
 
         $scope.puntoEntregaSelected = [];
@@ -231,6 +237,47 @@ function RechazoItemController($scope, EntitiesService) {
             }, null, $scope.onFail);
         }
     };
+
+    function onPuntoEntregaSelected(newValue, oldValue) {
+
+        if (newValue != null && newValue[0] !== undefined && newValue !== oldValue) {
+            $scope.supervisorVentasDS.read({
+                distritoId: $scope.distritoSelected.Key,
+                baseId: $scope.baseSelected.Key,
+                empleadoId: $scope.puntoEntregaSelected[0].ResponsableId
+            });
+        }
+    };
+
+    function onSupervisorVentasSelected(newValue, oldValue) {
+
+        if (newValue !== undefined && newValue !== oldValue) {
+            $scope.supervisorRutaDS.read({
+                distritoId: $scope.distritoSelected.Key,
+                baseId: $scope.baseSelected.Key,
+                empleadoId: $scope.supervisorVentasSelected.Key
+            });
+        }
+    };
+
+    function onSupervisorVentasDSLoad(e) {
+        if (e.type === "read" && e.response) {
+            $scope.supervisorVentasSelected = e.response[0];           
+        }
+    }
+
+    function onSupervisorRutaDSLoad(e) {
+        if (e.type === "read" && e.response) {
+           $scope.supervisorRutaSelected = e.response[0];
+        }
+    }
+    
+    //function onSupervisorVentasDSChange(newValue, oldValue)
+    //{
+    //    if (newValue !== oldValue) {
+    //        $scope.supervisorRutaSelected = [];
+    //    }
+    //}
 
     function isNew() { return $scope.operation === "A"; }
 
@@ -258,6 +305,10 @@ function RechazoItemController($scope, EntitiesService) {
 
     $scope.onSave = function() {
         $scope.rechazoWin.close();
+    };
+
+    function onFail(error) {
+        $scope.notify.show(error.errorThrown, "error");
     };
 }
 
