20d59b424a6076b565efe1aa6e9aa82ed8c2fee6
Matias Sisterna <mat_sis@hotmail.com>
2015-11-28 15:21:11 -0300

    Api para devolver los parámetros de un distrito

--- Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Controllers/api/DistritoController.cs
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Controllers/api/DistritoController.cs
@@ -10,10 +10,15 @@ namespace Logictracker.Web.Controllers.api
     public class DistritoController :EntityController<Empresa,EmpresaDAO,DistritoModel,EmpresaMapper>
     {
         [Route("api/Distrito/items")]
-        public  IEnumerable<ItemModel> GetComboItem()
+        public IEnumerable<ItemModel> GetComboItem()
         {
             return EntityDao.GetEmpresasPermitidas().Select(e=>Mapper.ToItem(e));
         }
-    }
 
+        [Route("api/Distrito/{distritoId}/parametros/items")]
+        public IEnumerable<KeyValuePair<string, string>> GetParametros(int distritoId)
+        {
+            return EntityDao.GetById(distritoId).Parametros.ToList().Select(p => new KeyValuePair<string, string>(p.Nombre, p.Valor));
+        }
+    }
 }
