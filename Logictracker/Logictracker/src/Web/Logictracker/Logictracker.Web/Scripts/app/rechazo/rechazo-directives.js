624e0e1a88401c4ef67849b73c85ff18c9ac681e
Matias Sisterna <mat_sis@hotmail.com>
2015-12-11 22:47:37 -0300

    Lo volví para atrás parece que hubo algún tema en los merge

--- Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/rechazo/rechazo-directives.js
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/rechazo/rechazo-directives.js
@@ -1,9 +1,19 @@
 ﻿angular.module('logictracker.rechazo.directives', [])
+.directive('ltRechazoEstado', function () {
+    return {
+        restrict: 'E',
+        template: '<input class="form-control" kendo-drop-down-list ' +
+                    'k-data-source="estadoDS" ' +
+                    'k-data-text-field="\'Value\'" ' +
+                    'k-data-value-field="\'Key\'" ' +
+                    'k-ng-model="estadoSelected" />'
+    };
+})
 
 .directive("ltPuntoEntrega", function () {
     return {
         restrict: "E",
-        template: '<input class="form-control" kendo-drop-down-list' +
+        template: '<input class="form-control" kendo-drop-down-list ' +
                     'k-data-source="" ' +
                     'k-data-text-field="\'Value\'" ' +
                     'k-data-value-field="\'Key\'" ' +
