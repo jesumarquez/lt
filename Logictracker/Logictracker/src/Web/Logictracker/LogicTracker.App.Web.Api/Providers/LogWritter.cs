974263e9839ccac04d2ffbf49912f359cfca0405
Tomas Valentin <tomas.valentin@logictracker.com>
2016-01-11 17:29:41 -0300

    Archivos no versionados

---
+++ Logictracker/Logictracker/src/Web/Logictracker/LogicTracker.App.Web.Api/Providers/LogWritter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace LogicTracker.App.Web.Api.Providers
+{
+    public class LogWritter
+    {
+        public static void writeLog(Exception error)
+        {
+            StringBuilder sb = new StringBuilder();           
+            sb.AppendLine(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString());
+            sb.AppendLine(error.Message);
+            sb.AppendLine(error.StackTrace.ToString());
+            sb.AppendLine("=======================================================================");
+            string filePath = HttpContext.Current.Server.MapPath("~");
+            File.AppendAllText(filePath+"log.txt", sb.ToString());
+            sb.Clear();
+        }
+    }
+}
