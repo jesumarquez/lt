e48257df512ac40f77e1909d6797a0c9a9938953
Jesu Marquez <jesumarquez@hotmail.com>
2016-01-13 00:04:41 -0300

    Cambios en la grilla de Ordenes.

--- Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/ordenes/ordenes-service.js
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Scripts/app/ordenes/ordenes-service.js
@@ -3,10 +3,46 @@
     .factory('OrdenesService', ['$resource', OrdenesService]);
 
 function OrdenesService($resource) {
-    return $resource("/api/distrito/:distritoId/base/:baseId/ordenes/:id",
-        { distritoId: "@distritoId", baseId: "@baseId", id: "@id" },
-        { query: { method: 'GET',  isArray: true }
-    });
+    var _service = {
+        items: getItems,
+        list: $resource("/api/distrito/:distritoId/base/:baseId/ordenes/:id",
+            { distritoId: "@distritoId", baseId: "@baseId", id: "@id" },
+            {
+                query: { method: 'GET', isArray: true }
+            })
+    }
+    
+    function getItems(data_, onEnd, onFail) {
+        var ds = new kendo.data.DataSource({
+            transport: {
+                read: {
+                    type: "GET",
+                    dataType: "json",
+                    url: "/api/distrito/" + data_.distritoId + "/base/" + data_.baseId + "/ordenes/"
+                }
+            },
+            schema: {
+                total: function (response) {
+                    return response.length;
+                }
+            },
+            pageSize: 25,
+        });
+
+        if (angular.isFunction(onEnd))
+            ds.bind("requestEnd", onEnd);
+        if (angular.isFunction(onFail))
+            ds.bind("error", onFail);
+
+        return ds;
+    }
+
+    return _service;
+
+    //return $resource("/api/distrito/:distritoId/base/:baseId/ordenes/:id",
+    //    { distritoId: "@distritoId", baseId: "@baseId", id: "@id" },
+    //    { query: { method: 'GET',  isArray: true }
+    //});
 }
 
 
