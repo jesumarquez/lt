ff8e6f10467f0c1a6006e3cd940e661be7691398
Jesu Marquez <jesumarquez@hotmail.com>
2016-01-27 12:25:54 -0300

    Faltaba agregar archivos
    Related Work Items: #196

---
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Areas/Eventos/Controllers/EventosController.cs
@@ -0,0 +1,28 @@
+﻿using Logictracker.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Logictracker.Web.Areas.Eventos.Controllers
+{
+    public class EventosController : BaseFunctionController, IFunctionController
+    {
+        public string VariableName
+        {
+            get { return "PAR_EVENTOS"; }
+        }
+
+        public string GetRefference()
+        {
+            return "PAR_EVENTOS";
+        }
+
+        // GET: Eventos/Eventos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
