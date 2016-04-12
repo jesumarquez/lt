b01bf717e4b2e9d0d0fa4677426723ca53e6b1ac
Matias Sisterna <mat_sis@hotmail.com>
2016-04-12 18:52:38 -0300

    Ahora la base devuelve la referencia geografica pero no esta bien resuelto la manera en como obtiene la referencia geográfica. La api deja de devolver un ItemModel para devolver un BaseModel pero conserva las propiedades Key y Value para no tener que cambiar la parte Cliente.

--- Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Models/BaseModel.cs
+++ Logictracker/Logictracker/src/Web/Logictracker/Logictracker.Web/Models/BaseModel.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using Logictracker.Types.BusinessObjects;
+using Logictracker.DAL.Factories;
 
 namespace Logictracker.Web.Models
 {
     public class BaseModel
     {
         public int BaseId { get; set; }
-        public int EmpresaId  { get; set; }
+        public string Descripcion { get; set; }
+        public int EmpresaId { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public int Key { get; set; }
+        public string Value { get; set; }
     }
 
     public class LineaMapper : EntityModelMapper<Linea, BaseModel>
     {
+        static DAOFactory daoF = new DAOFactory();
+
         public override BaseModel EntityToModel(Linea entity, BaseModel model)
         {
-            throw new NotImplementedException();
+            model.Key = entity.Id;
+            model.EmpresaId = entity.Empresa.Id;
+            model.Value = entity.Descripcion;
+            model.Latitud = entity.ReferenciaGeografica != null ? daoF.ReferenciaGeograficaDAO.FindById(entity.ReferenciaGeografica.Id).Latitude : 0;
+            model.Longitud = entity.ReferenciaGeografica != null ? daoF.ReferenciaGeograficaDAO.FindById(entity.ReferenciaGeografica.Id).Longitude : 0;
+
+            return model;
         }
 
         public override Linea ModelToEntity(BaseModel model, Linea entity)
@@ -23,7 +37,7 @@ namespace Logictracker.Web.Models
 
         public override ItemModel ToItem(Linea entity)
         {
-            return new ItemModel { Key = entity.Id, Value = entity.Descripcion};
+            return new ItemModel { Key = entity.Id, Value = entity.Descripcion };
         }
     }
 }
