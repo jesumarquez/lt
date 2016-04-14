2bfd287626123253d90a8b219d125c6deb5d4a5f
Lucas <metzler.lucas@gmail.com>
2016-04-14 18:02:04 -0300

    Monitor Check In / Out Reporte In / Out Nomenclador Masivo Bug AutoComplete Agenda Vehicular

--- Logictracker/SQL Scripts/logictracker/logictracker.20160411.lmetzler.02.VW_BI_VIAJE_RECARGA.sql
+++ Logictracker/SQL Scripts/logictracker/logictracker.20160411.lmetzler.02.VW_BI_VIAJE_RECARGA.sql
@@ -1,7 +1,7 @@
 USE [logictracker]
 GO
 
-/****** Object:  View [dbo].[VW_BI_VIAJE_RECARGA]    Script Date: 11/04/2016 11:28:31 ******/
+/****** Object:  View [dbo].[VW_BI_VIAJE_RECARGA]    Script Date: 12/04/2016 16:01:59 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -17,10 +17,10 @@ SELECT	 [id_opetick17]
 		,[rela_parenti03]
 		,[opetick17_interno]
 		,[opetick17_patente]
-		,[opetick17_fecha]
+		,CAST([opetick17_fecha] AS DATE) AS 'opetick17_fecha'
 		,[opetick17_accion]
-		,[opetick17_inicio]
-		,[opetick17_fin]
+		,CAST([opetick17_inicio] AS TIME) AS 'opetick17_inicio'
+		,CAST([opetick17_fin] AS TIME) AS 'opetick17_fin'
 		,[opetick17_duracion]
 FROM    dbo.[ope.ope_tick_17_viaje_recarga]
 WHERE [rela_parenti01] IN (	SELECT [su10].RELA_PARENTI01
@@ -29,119 +29,6 @@ WHERE [rela_parenti01] IN (	SELECT [su10].RELA_PARENTI01
 									ON (([su10].rela_socusua01 = [su13].rela_socusua01 AND [su13].[socusua13_parametro] = 'bi_login' AND [su13].[socusua13_valor] = SYSTEM_USER) OR SYSTEM_USER = 'sa')	)
 
 
-GO
-
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
-Begin DesignProperties = 
-   Begin PaneConfigurations = 
-      Begin PaneConfiguration = 0
-         NumPanes = 4
-         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
-      End
-      Begin PaneConfiguration = 1
-         NumPanes = 3
-         Configuration = "(H (1 [50] 4 [25] 3))"
-      End
-      Begin PaneConfiguration = 2
-         NumPanes = 3
-         Configuration = "(H (1 [50] 2 [25] 3))"
-      End
-      Begin PaneConfiguration = 3
-         NumPanes = 3
-         Configuration = "(H (4 [30] 2 [40] 3))"
-      End
-      Begin PaneConfiguration = 4
-         NumPanes = 2
-         Configuration = "(H (1 [56] 3))"
-      End
-      Begin PaneConfiguration = 5
-         NumPanes = 2
-         Configuration = "(H (2 [66] 3))"
-      End
-      Begin PaneConfiguration = 6
-         NumPanes = 2
-         Configuration = "(H (4 [50] 3))"
-      End
-      Begin PaneConfiguration = 7
-         NumPanes = 1
-         Configuration = "(V (3))"
-      End
-      Begin PaneConfiguration = 8
-         NumPanes = 3
-         Configuration = "(H (1[56] 4[18] 2) )"
-      End
-      Begin PaneConfiguration = 9
-         NumPanes = 2
-         Configuration = "(H (1 [75] 4))"
-      End
-      Begin PaneConfiguration = 10
-         NumPanes = 2
-         Configuration = "(H (1[66] 2) )"
-      End
-      Begin PaneConfiguration = 11
-         NumPanes = 2
-         Configuration = "(H (4 [60] 2))"
-      End
-      Begin PaneConfiguration = 12
-         NumPanes = 1
-         Configuration = "(H (1) )"
-      End
-      Begin PaneConfiguration = 13
-         NumPanes = 1
-         Configuration = "(V (4))"
-      End
-      Begin PaneConfiguration = 14
-         NumPanes = 1
-         Configuration = "(V (2))"
-      End
-      ActivePaneConfig = 0
-   End
-   Begin DiagramPane = 
-      Begin Origin = 
-         Top = 0
-         Left = 0
-      End
-      Begin Tables = 
-         Begin Table = "ope.ope_tick_17_viaje_recarga"
-            Begin Extent = 
-               Top = 6
-               Left = 38
-               Bottom = 135
-               Right = 230
-            End
-            DisplayFlags = 280
-            TopColumn = 0
-         End
-      End
-   End
-   Begin SQLPane = 
-   End
-   Begin DataPane = 
-      Begin ParameterDefaults = ""
-      End
-   End
-   Begin CriteriaPane = 
-      Begin ColumnWidths = 11
-         Column = 1440
-         Alias = 900
-         Table = 1170
-         Output = 720
-         Append = 1400
-         NewValue = 1170
-         SortType = 1350
-         SortOrder = 1410
-         GroupBy = 1350
-         Filter = 1350
-         Or = 1350
-         Or = 1350
-         Or = 1350
-      End
-   End
-End
-' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'VW_BI_VIAJE_RECARGA'
-GO
-
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'VW_BI_VIAJE_RECARGA'
 GO
 
 
